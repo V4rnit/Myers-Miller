0298e35fcc6180697242d3b26bd8197a465f704a
Varnit <121328381+V4rnit@users.noreply.github.com>
2026-09-13 23:04:26 +0000

    correct implementaion of gotoh Algo

--- gotoh.c
+++ gotoh.c
@@ -2,35 +2,90 @@
 #include <stdlib.h>
 #include <string.h>
 #define INF 10000
+int getMin2(int a, int b){
+    return (a < b) ? a : b;
+}
 
-int getMin(int a, int b, int c){
+int getMin3(int a, int b, int c){
     int min = a;
     if(b < a) min = b;
     return(min < c) ? min : c;
 }
-int replace(char a, char b, int mismatchCost){
+// GA = gap in A
+// GB = gap in B
 
-    if(a == b) return 0;
-    return mismatchCost;
-}
-int gotoh(char* A, char* B, int gapOpen, int gapExtend){
+int gotoh(char* A, char* B, int gapOpen, int gapExtend, int misMatch){
     int m = strlen(A);
     int n = strlen(B);
 
-    int C [m + 1][n + 1];
-    int GA [m + 1][n + 1];
-    int GB [m + 1][n + 1];
+    int C[m + 1][n + 1];
+    int GA[m + 1][n + 1];
+    int GB[m + 1][n + 1];
 
     C[0][0] = 0;
-    C[0][1] = gapOpen;
-    C[1][0] = gapOpen;
     GA[0][0] = INF;
     GB[0][0] = INF;
 
-    for(int i = 2; i < m; i++){
-        C[i][0] = C[i - 1][0] + gapExtend;
+    // First column: B is empty, so gaps are in B
+    for(int i = 1; i <= m; i++){
+        if(i == 1){
+            C[i][0] = gapOpen;
+        }
+        else{
+            C[i][0] = C[i - 1][0] + gapExtend;
+        }
+
+        GA[i][0] = INF;
+        GB[i][0] = C[i][0];
     }
-    for(int j = 2; j < n; j++){
-        C[0][j] = C[j - 1][0] + gapExtend;
+
+    // First row: A is empty, so gaps are in A
+    for(int j = 1; j <= n; j++){
+        if(j == 1){
+            C[0][j] = gapOpen;
+        }
+        else{
+            C[0][j] = C[0][j - 1] + gapExtend;
+        }
+
+        GA[0][j] = C[0][j];
+        GB[0][j] = INF;
     }
+
+    // Fill the matrices
+    for(int i = 1; i <= m; i++){
+        for(int j = 1; j <= n; j++){
+
+            // Gap in A -> move left
+            GA[i][j] = getMin2(
+                GA[i][j - 1] + gapExtend,
+                C[i][j - 1] + gapOpen
+            );
+
+            // Gap in B -> move up
+            GB[i][j] = getMin2(
+                GB[i - 1][j] + gapExtend,
+                C[i - 1][j] + gapOpen
+            );
+
+            // Match / mismatch -> move diagonal
+            int diagonal;
+
+            if(A[i - 1] == B[j - 1]){
+                diagonal = C[i - 1][j - 1];
+            }
+            else{
+                diagonal = C[i - 1][j - 1] + misMatch;
+            }
+
+            // Best way to reach this cell
+            C[i][j] = getMin3(
+                diagonal,
+                GA[i][j],
+                GB[i][j]
+            );
+        }
+    }
+
+    return C[m][n];
 }
